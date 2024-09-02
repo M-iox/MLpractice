d3ce50d101619fcd794bd68ee68997b11ed96536
M-iox <1251626309@QQ.COM>
2024-09-02 16:03:46 +0800

    初版

--- 3新冠肺炎感染者的临床数据/main.py
+++
@@ -1,5 +0,0 @@
-from regression import RandomForestRegressionModel
-
-RegressionModel = RandomForestRegressionModel(data_path='训练集.xlsx')
-RegressionModel.tune_hyperparameters()
-RegressionModel.cross_validate()
